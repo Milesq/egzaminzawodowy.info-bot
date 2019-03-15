a209687b35e37918c0a65c272473c4b9950d2ba7
Milesq <wmilosz88@gmail.com>
2019-03-15 18:52:35 +0100

    Remove the possibility of sending tests multiple times

--- src/js/main.js
+++ src/js/main.js
@@ -28,25 +28,21 @@ if (typeof TESTS === 'undefined') {
         ev.preventDefault();
         const max = document.getElementById('max-percents').value;
         const min = document.getElementById('min-percents').value;
-        let iters = document.getElementById('iterations').value;
 
         // eslint-disable-next-line
-        for(;iters>0;--iters) {
-            chrome.tabs.query({
-                url: 'https://www.testy.egzaminzawodowy.info/question.php*'
-            // eslint-disable-next-line no-loop-func
-            }, ([tab]) => {
-                if (tab === undefined) {
-                    alert('Najpierw otwórz kartę z egzaminem');
-                    openUrl('https://www.testy.egzaminzawodowy.info/technik-informatyk');
-                    return;
-                }
+        chrome.tabs.query({
+            url: 'https://www.testy.egzaminzawodowy.info/question.php*'
+        // eslint-disable-next-line no-loop-func
+        }, ([tab]) => {
+            if (tab === undefined) {
+                alert('Najpierw otwórz kartę z egzaminem');
+                openUrl('https://www.testy.egzaminzawodowy.info/technik-informatyk');
+                return;
+            }
 
-                console.log(rand(min, max));
-                // chrome.runtime.sendMessage({
-                //     percents: rand(min, max)
-                // });
+            chrome.runtime.sendMessage({
+                percents: rand(min, max)
             });
-        }
+        });
     });
 }
